8a61245111453a60a93fc10161228110a3c6ee23
colewrus <wrampelc@gmail.com>
2018-02-01 00:25:00 -0500

    guest AI first pass

---
+++ Assets/guest/Scripts/GM_guestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Section
+{
+    public List<GameObject> installations = new List<GameObject>();
+
+    public Transform entrance;
+}
+
+public class GM_guestScript : MonoBehaviour {
+
+    public static GM_guestScript instance = null;
+
+    public List<Section> Wings = new List<Section>();
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
