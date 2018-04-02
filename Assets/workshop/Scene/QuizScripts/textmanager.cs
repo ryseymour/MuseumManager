1f0945c148d83466faae21331658bbb08d4907a9
ryseymour <ry.seymour@gmail.com>
2018-04-02 19:51:45 -0400

    update

---
+++ Assets/workshop/Scene/QuizScripts/textmanager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textmanager : MonoBehaviour {
+
+	List<string> questions = new List<string>() {"First question", "Second question", "Third question", "Fourth Question", "Fifth Question"};
+
+	List<string> firstchoice = new List<string>() {"first choice1","first choice2","first choice3", "first choice 4", "first choice 5" };
+	List<string> secondchoice = new List<string>() {"second choice" ,"second choice2","secondchoice3", "second choice 4", "secondchoice 5"};
+	List<string> thirdchoice = new List<string>() {"third choice","third choice2","third choice3", "third choice 4", "third choice 5"};
+	List<string> fourthchoice = new List<string>() {"fourth choice", "fourth choice2","fourth choice3", "fourth choice 4", "fourth choice 5"};
+
+	public GameObject OneQuestion;
+	public GameObject TwoQuestion;
+	public GameObject ThreeQuestion;
+	public GameObject FourQuestion;
+
+	public static int randQuestion;
+
+	List<string> correctAnswer = new List<string>() {"4","1","2","4","3"};
+
+	public static string selectedAnswer;
+
+	public static bool choiceSelected;
+
+	// Use this for initialization
+	void Start () {
+		//GetComponent<TextMesh> ().text = questions [0];
+		//OneQuestion.GetComponent<TextMesh> ().text = firstchoice [0];
+		//TwoQuestion.GetComponent<TextMesh> ().text = secondchoice [0];
+		//ThreeQuestion.GetComponent<TextMesh> ().text = thirdchoice [0];
+		//FourQuestion.GetComponent<TextMesh> ().text = fourthchoice [0];
+
+		QuestionRand ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void QuestionRand (){
+		randQuestion = Random.Range (0, 5);
+		QuestionPop ();
+	}
+
+	void QuestionPop (){
+		GetComponent<TextMesh> ().text = questions [randQuestion];
+		OneQuestion.GetComponent<TextMesh> ().text = firstchoice [randQuestion];
+		TwoQuestion.GetComponent<TextMesh> ().text = secondchoice [randQuestion];
+		ThreeQuestion.GetComponent<TextMesh> ().text = thirdchoice [randQuestion];
+		FourQuestion.GetComponent<TextMesh> ().text = fourthchoice [randQuestion];
+	}
+
+	public void Answer (){
+		if (choiceSelected == true) {
+
+			choiceSelected = false;
+
+			if (correctAnswer [randQuestion] == selectedAnswer) {
+
+				QuestionRand ();
+
+			} else if (correctAnswer [randQuestion] != selectedAnswer) {
+				Debug.Log ("incorrect");
+			}
+		}
+	}
+
+
+}
