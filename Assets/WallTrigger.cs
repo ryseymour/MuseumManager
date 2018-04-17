6f22b8fd669efb427854474ef48414f3f4890692
sourseymour <ry.seymour@gmail.com>
2018-04-17 10:40:56 -0400

    update with walls!

---
+++ Assets/WallTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrigger : MonoBehaviour {
+	public GameObject Cam;
+	public Transform Startspot;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter ()
+	{
+		Cam.gameObject.transform.position = Startspot.transform.position;
+		Debug.Log ("transform");
+	}
+}
--- Assets/ylock.cs
+++ Assets/ylock.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ylock : MonoBehaviour {
 	public Camera C1;
 	public float Cpos= 5.48f;
+	public Transform resetpos;
 
 	// Use this for initialization
 	void Start () {
@@ -16,4 +17,12 @@ public class ylock : MonoBehaviour {
 		Vector3 pos = new Vector3 (transform.position.x, 5.48f, transform.position.z);
 		Mathf.Clamp (transform.position.y, 5.47f, 5.49f);
 	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Wall") {
+			Debug.Log ("hit");
+			this.gameObject.transform.position = resetpos.position;
+			//other.transform.position = resetpos.position;
+		}
+}
 }
