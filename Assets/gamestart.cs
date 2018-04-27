5be1ffe420f4cd210d9c2af2bc45596d4ab49112
ryseymour <ry.seymour@gmail.com>
2018-04-26 23:27:45 -0400

    added opening scene

---
+++ Assets/gamestart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamestart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void StartButton() {
+		SceneManager.LoadScene ("test", LoadSceneMode.Single);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
