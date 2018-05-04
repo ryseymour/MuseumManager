16965e939433167345fba69c26c3ef93856ddf3a
ryseymour <ry.seymour@gmail.com>
2018-05-03 22:17:33 -0400

    infosheets
    added in infosheets, one more push for start screen and then complete.

---
+++ Assets/HelpScreenScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScreenScript : MonoBehaviour {
+	public GameObject HelpScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown (){
+		HelpScreen.SetActive (true);
+		Debug.Log ("HelpScreenTest");
+	}
+}
